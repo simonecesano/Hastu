b918259fa8a4df70070ebb81ed59d1772741e4c7
simone cesano <simone.cesano@gmail.com>
2014-10-31 23:19:29 +0100

    working on Oauth

--- lib/Hastu/Controller/Google.pm
+++ lib/Hastu/Controller/Google.pm
@@ -13,11 +13,11 @@ my $auth = Net::OAuth2::Profile::WebServer->new
      client_id      => '1042989076422-g03hljhmda7jne9jot3j526taf77i345.apps.googleusercontent.com',
      client_secret  => 'iVDphllBU8pE-5jYMVZkytOH',
      site           => 'https://accounts.google.com',
-     scope          => 'https://www.googleapis.com/auth/userinfo.email',
+     scope          => 'https://www.googleapis.com/auth/userinfo.profile',
      authorize_path    => '/o/oauth2/auth',
      access_token_path => '/o/oauth2/token',
      redirect_uri      => 'http://hastu.herokuapp.com/google/inst',
-     protected_resource_url => 'https://www.google.com/m8/feeds/contacts/default/full'
+     protected_resource_url => 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json'
     );
 
 
@@ -36,7 +36,7 @@ sub inst :Path('inst') {
     my ($self, $c) = @_;
 
     my $access_token  = $auth->get_access_token($c->req->params->{code});
-    $c->res->body(join "\n", '<pre>', (dump $access_token), '</pre>');
+    $c->res->body(join "\n", '<pre>', (dump $access_token->get('')), '</pre>');
     
 }
 
