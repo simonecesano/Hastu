8991a2425cea42b936bf8e00a1607b6de8f427c2
simone cesano <simone.cesano@gmail.com>
2014-11-01 01:28:03 +0100

    working on Oauth

--- lib/Hastu/Controller/Google.pm
+++ lib/Hastu/Controller/Google.pm
@@ -9,16 +9,16 @@ BEGIN { extends 'Catalyst::Controller'; }
 
 my $auth = Net::OAuth2::Profile::WebServer->new
     ( 
-     name           => 'Google Contacts',
-     client_id      => '1042989076422-g03hljhmda7jne9jot3j526taf77i345.apps.googleusercontent.com',
-     client_secret  => 'iVDphllBU8pE-5jYMVZkytOH',
-     site           => 'https://accounts.google.com',
-     scope          => 'https://www.googleapis.com/auth/userinfo.email',
+     name              => 'Google Contacts',
+     client_id         => '1042989076422-g03hljhmda7jne9jot3j526taf77i345.apps.googleusercontent.com',
+     client_secret     => 'iVDphllBU8pE-5jYMVZkytOH',
+     site              => 'https://accounts.google.com', 
+     scope             => 'https://www.google.com/m8/feeds/',
      authorize_path    => '/o/oauth2/auth',
      access_token_path => '/o/oauth2/token',
      redirect_uri      => 'http://hastu.herokuapp.com/google/inst',
-     protected_resource_url => 'https://www.google.com/m8/feeds/contacts/default/full'
-    );
+     protected_resource_url
+        =>  'https://www.google.com/m8/feeds/contacts/default/full'    );
 
 
 sub index :Path :Args(0) {
@@ -35,9 +35,13 @@ sub login :Path('login') {
 sub inst :Path('inst') {
     my ($self, $c) = @_;
 
-    my $access_token  = $auth->get_access_token($c->req->params->{code});
-    $c->res->body(join "\n", '<pre>', (dump $access_token), '</pre>');
-    
+    if (0) {
+	my $access_token  = $auth->get_access_token($c->req->params->{code});
+	$c->res->body(join "\n", '<pre>', (dump $access_token), '</pre>');
+    }
+    if (1) {
+	my $code = $c->req->params->{code};
+    }
 }
 
 
