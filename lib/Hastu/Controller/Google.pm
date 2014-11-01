251a625f892076c65d5934654358fc710632aef1
simone cesano <simone.cesano@gmail.com>
2014-11-01 01:15:20 +0100

    working on Oauth

--- lib/Hastu/Controller/Google.pm
+++ lib/Hastu/Controller/Google.pm
@@ -13,15 +13,11 @@ my $auth = Net::OAuth2::Profile::WebServer->new
      client_id      => '1042989076422-g03hljhmda7jne9jot3j526taf77i345.apps.googleusercontent.com',
      client_secret  => 'iVDphllBU8pE-5jYMVZkytOH',
      site           => 'https://accounts.google.com',
-     scope          => join ' ', qw|
-				     https://www.googleapis.com/auth/plus.info
-				     https://www.googleapis.com/auth/plus.me
-				     https://www.googleapis.com/auth/userinfo.email
-				   |,
+     scope          => 'https://www.googleapis.com/auth/userinfo.email',
      authorize_path    => '/o/oauth2/auth',
      access_token_path => '/o/oauth2/token',
      redirect_uri      => 'http://hastu.herokuapp.com/google/inst',
-     protected_resource_url => 'https://www.googleapis.com/plus/v1/people'
+     protected_resource_url => 'https://www.google.com/m8/feeds/contacts/default/full'
     );
 
 
