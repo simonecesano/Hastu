9560dc27fff16214a18e9a6703888fc561602d21
simone cesano <simone.cesano@gmail.com>
2014-11-01 01:14:07 +0100

    working on Oauth

--- lib/Hastu/Controller/Google.pm
+++ lib/Hastu/Controller/Google.pm
@@ -12,17 +12,16 @@ my $auth = Net::OAuth2::Profile::WebServer->new
      name           => 'Google Contacts',
      client_id      => '1042989076422-g03hljhmda7jne9jot3j526taf77i345.apps.googleusercontent.com',
      client_secret  => 'iVDphllBU8pE-5jYMVZkytOH',
+     site           => 'https://accounts.google.com',
      scope          => join ' ', qw|
 				     https://www.googleapis.com/auth/plus.info
 				     https://www.googleapis.com/auth/plus.me
 				     https://www.googleapis.com/auth/userinfo.email
-				     https://www.googleapis.com/auth/userinfo.profile
 				   |,
-     site              => 'https://accounts.google.com',
      authorize_path    => '/o/oauth2/auth',
      access_token_path => '/o/oauth2/token',
      redirect_uri      => 'http://hastu.herokuapp.com/google/inst',
-     # protected_resource_url => 'https://www.googleapis.com/plus/v1/people'
+     protected_resource_url => 'https://www.googleapis.com/plus/v1/people'
     );
 
 
@@ -40,8 +39,8 @@ sub login :Path('login') {
 sub inst :Path('inst') {
     my ($self, $c) = @_;
 
-    my $token  = $auth->get_access_token($c->req->params->{code});
-    $c->res->body(join "\n", '<pre>', (dump $token->get('https://www.googleapis.com/oauth2/v2/userinfo')), '</pre>');
+    my $access_token  = $auth->get_access_token($c->req->params->{code});
+    $c->res->body(join "\n", '<pre>', (dump $access_token), '</pre>');
     
 }
 
